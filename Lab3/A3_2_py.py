0fc2a139ed5dca062e00655ce4042731f65818af
Kunal Kanade <43581648+KandyKad@users.noreply.github.com>
2020-02-20 01:46:42 +0530

    Code2

---
+++ Lab3/A3_2_py.py
@@ -0,0 +1,12 @@
+mylist1=[10,345,565,44,4,34,22,64,561,43]
+print("list1 : ", mylist1)
+mylist2=[]
+print("list2 : ", mylist2)
+if mylist1:
+    print("list1 is non empty")
+else:
+    print("list1 is empty")
+if mylist2:
+    print("list2 is non empty")
+else:
+    print("list2 is empty")
