4da734bf748e122ce7389a05908efbcbf2d1e66b
Kunal Kanade <43581648+KandyKad@users.noreply.github.com>
2020-02-20 02:23:37 +0530

    Delete A1_3py_Area of circle.py

--- Lab1/A1_3py_Area of circle.py
+++
@@ -1,7 +0,0 @@
-import math
-PI = math.pi
-Radius = int(input("Enter the radius of the circle:"))
-Area = PI * Radius * Radius
-Circum = 2 * PI * Radius
-print("Area of circle is:", Area)
-print("Circumference of circle is:", Circum)
