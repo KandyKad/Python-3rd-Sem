6930f39ffe48665e5599344af1520cf830b41081
Kunal Kanade <43581648+KandyKad@users.noreply.github.com>
2020-02-20 01:50:36 +0530

    Code8

---
+++ Lab3/A3_8_py.py
@@ -0,0 +1,7 @@
+mylist1=[10,20,30,40]
+print("Given list1 : ", mylist1)
+mylist2=[101,201,301,401]
+print("Given list2 : ", mylist2)
+mylist1.pop()
+mylist1.extend(mylist2)
+print("New list : ",mylist1)
