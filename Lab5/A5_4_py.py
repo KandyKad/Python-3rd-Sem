af5e53b509dceda88ce267b737e047aa8cdbe63b
Kunal Kanade <43581648+KandyKad@users.noreply.github.com>
2020-02-21 21:00:15 +0530

    Code4

---
+++ Lab5/A5_4_py.py
@@ -0,0 +1,16 @@
+a = input("Enter the words in a comma sperated fashion: ").split(",")
+a.sort()
+list1 = [i for i in a]
+print(tuple(list1))
+
+
+
+
+
+'''
+while(true)
+a = input
+    if a:
+        new = a.split
+        else:
+            break'''
